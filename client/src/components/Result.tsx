07be6ba30e4b2ddfc3c638c0335ea14aaa84bad2
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-22 16:54:40 +0900

    update

--- client/src/components/Result.tsx
+++ client/src/components/Result.tsx
@@ -1,7 +1,43 @@
 import React from 'react'
+import PageLayout from './common/PageLayout'
+import '../styles/Result.scss'
+import { Link } from 'react-router-dom'
 
 const Result = () => {
-  return <div>Result</div>
+  const onReset = () => {}
+
+  return (
+    <PageLayout>
+      <div className="result flex-center">
+        <div className="flex">
+          <span>お名前：</span>
+          <span className="bold">Sample太郎</span>
+        </div>
+        <div className="flex">
+          <span>問題総数：</span>
+          <span className="bold">5</span>
+        </div>
+        <div className="flex">
+          <span>回答数：</span>
+          <span className="bold">3</span>
+        </div>
+        <div className="flex">
+          <span>獲得得点</span>
+          <span className="bold">50</span>
+        </div>
+        <div className="flex">
+          <span>最終結果</span>
+          <span className="bold">合格</span>
+        </div>
+      </div>
+
+      <div className="start">
+        <Link className="btn" to={'/'} onClick={onReset}>
+          再挑戦
+        </Link>
+      </div>
+    </PageLayout>
+  )
 }
 
 export default Result
