fb35f3492cd0865a0d8ab288c2e3b05b2214450a
Tahara Kazuki <ssp0727.lnc@gmail.com>
2022-11-23 17:42:57 +0900

    update

--- client/src/database/data.ts
+++ client/src/database/data.ts
@@ -6,23 +6,23 @@ const data = [
   },
   {
     id: 2,
-    question: 'Following methods can be used to display data in some form using Javascript',
-    options: ['document.write()', 'console.log()', 'window.alert()'],
+    question: 'xxxxxxxxxxxx',
+    options: ['A：11月11日', 'B：2月20日', 'C：7月10日', 'D：10月25日'],
   },
   {
     id: 3,
-    question: 'When an operator value is NULL, the typeof returned by the unary operator is:',
-    options: ['Boolean', 'Undefined', 'Object'],
+    question: 'xxxxxxxxxxxx',
+    options: ['A：11月11日', 'B：2月20日', 'C：7月10日', 'D：10月25日'],
   },
   {
     id: 4,
-    question: 'What does the toString() method return?',
-    options: ['Return Object', 'Return String', 'Return Integer'],
+    question: 'xxxxxxxxxxxx',
+    options: ['A：11月11日', 'B：2月20日', 'C：7月10日', 'D：10月25日'],
   },
   {
     id: 5,
-    question: 'Which function is used to serialize an object into a JSON string?',
-    options: ['stringify()', 'parse()', 'convert()'],
+    question: 'xxxxxxxxxxxx',
+    options: ['A：11月11日', 'B：2月20日', 'C：7月10日', 'D：10月25日'],
   },
 ]
 
